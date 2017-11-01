0a175ec236c19380482604a5aeb8987dff7efe39
André Formento <andreformento.sc@gmail.com>
2017-11-01 20:07:39 -0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,16 +3,21 @@
 mkdir ~/init
 cd ~/init
 
-git clone git@github.com:andreformento/linux-local-configs.git
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+################################### 
 export WS="$HOME/ws" && \
 mkdir -p $WS && \
 mkdir -p $HOME/.ssh && \
 git config --global user.name "andreformento" && \
 git config --global user.email "andreformento.sc@gmail.com" && \
 git config --global push.default simple
+
+
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+git clone git@github.com:andreformento/linux-local-configs.git
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+################################### 
 
 cd ~
 rm -rf ~/init
