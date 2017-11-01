760682f478cb01b6921aef286a4bab5b52289fb3
André Formento <andreformento.sc@gmail.com>
2017-11-01 20:24:10 -0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 
 docker run \
        -it \
-       -v $(pwd)/ws:/root/ws \
+       -v $(pwd)/dev:/root/dev \
        -v $(pwd)/ssh:/root/.ssh \
        -e USERID=$UID \
        andreformento/workspace-machine /bin/zsh
