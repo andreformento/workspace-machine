ef6fac1895184bf6f2a8d5d367e6f670d53b117f
André Formento <andreformento.sc@gmail.com>
2017-11-02 16:52:02 -0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,7 +7,15 @@ git config --global user.name "andreformento"
 git config --global user.email "andreformento.sc@gmail.com"
 git config --global push.default simple
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+# Configure oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i -e "s/bin\/ash/bin\/zsh/" /etc/passwd
-#git clone git@github.com:andreformento/linux-local-configs.git
+
+# Customize my terminal
+
+curl -fsSL https://raw.github.com/andreformento/linux-local-configs/master/agnoster.zsh-theme
+chmod u+x agnoster.zsh-theme
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/andreformento/linux-local-configs/master/init.sh)"
+
 echo "ok"
